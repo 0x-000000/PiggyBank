0e11d3d7ceca36401a568fde99c673fd25e4f57b
cbulkin <chasebulkin@yahoo.com>
2025-11-17 21:35:27 -0700

    Tax Evasion

---
+++ TaxCalculator/Models/TaxInfoInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaxCalculator.Models
+{
+    public class TaxInfoInput
+    {
+        public decimal Income { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ TaxCalculator/Models/TaxResults.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaxCalculator.Models
+{
+    public class TaxResults
+    {
+        public decimal FederalTax { get; set; }
+        public decimal StateTax { get; set; }
+        public decimal TotalTax => FederalTax + StateTax;
+        public decimal NetIncome { get; set; }
+    }
+}
